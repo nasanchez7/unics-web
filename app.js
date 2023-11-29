f17fe36cf87de26462ee48dc8798341df84d803f
nasanchez7 <nadirblanco02@gmail.com>
2023-11-29 11:00:38 -0300

    avanzando en tarjetas de profesionales

--- app.js
+++ app.js
@@ -22,3 +22,66 @@ if(links){
         })
     }
 }
+
+const profesionales = [ 
+    {
+       nombre: "MARI",
+       imagenSinFondo: "./img/mariCarmen.png",
+       imagenConFondo: "",
+       iframeLink: "" 
+    },
+    {
+        nombre: "CAROLINA",
+        imagenSinFondo: "./img/Carolina.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "AURORA",
+        imagenSinFondo: "./img/aurora.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "MARIANA",
+        imagenSinFondo: "./img/Mariana.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "JESSICA",
+        imagenSinFondo: "./img/jesi.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "MARIA",
+        imagenSinFondo: "./img/Maria.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "SARA",
+        imagenSinFondo: "./img/Sara.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "SONIA",
+        imagenSinFondo: "./img/Sonia.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "JAVIER",
+        imagenSinFondo: "./img/Javier.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+     {
+        nombre: "MARI",
+        imagenSinFondo: "./img/mariCarmenSecre.png",
+        imagenConFondo: "",
+        iframeLink: "" 
+     },
+]
